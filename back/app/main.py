7bb6e472cdd24534debd71aa0eaa99d2d12a3c87
Arian Rodrigo Reyes Diestra <areyes@promhil.sac>
2026-02-13 09:44:55 -0500

    asdsad

--- back/app/main.py
+++ back/app/main.py
@@ -63,7 +63,7 @@ async def add_security_headers(request: Request, call_next):
     response = await call_next(request)
     # Headers de seguridad
     response.headers["X-Content-Type-Options"] = "nosniff"
-    response.headers["X-Frame-Options"] = "DENY"
+
     response.headers["X-XSS-Protection"] = "1; mode=block"
     response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
     response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
